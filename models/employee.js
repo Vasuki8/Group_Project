f6a1952a732aefebfe92f4352b8f0d801d73e685
Yug3010 <yugsutariya4i@gmail.com>
2024-04-09 11:23:10 -0400

    Modify Display UI

--- models/employee.js
+++ models/employee.js
@@ -1,42 +1,42 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+  const mongoose = require('mongoose');
+  const Schema = mongoose.Schema;
 
-const movieSchema = new Schema({
-  plot: String,
-  genres: [String],
-  runtime: Number,
-  cast: [String],
-  num_mflix_comments: Number,
-  title: String,
-  fullplot: String,
-  languages: [String],
-  released: Date,
-  directors: [String],
-  rated: String,
-  awards: {
-    wins: Number,
-    nominations: Number,
-    text: String
-  },
-  lastupdated: Date,
-  year: Number,
-  imdb: {
-    rating: Number,
-    votes: Number,
-    id: Number
-  },
-  countries: [String],
-  type: String,
-  tomatoes: {
-    viewer: {
-      rating: Number,
-      numReviews: Number,
-      meter: Number
+  const movieSchema = new Schema({
+    plot: String,
+    genres: [String],
+    runtime: Number,
+    cast: [String],
+    num_mflix_comments: Number,
+    title: String,
+    fullplot: String,
+    languages: [String],
+    released: Date,
+    directors: [String],
+    rated: String,
+    awards: {
+      wins: Number,
+      nominations: Number,
+      text: String
     },
-    lastUpdated: Date
-  }
-});
+    lastupdated: Date,
+    year: Number,
+    imdb: {
+      rating: Number,
+      votes: Number,
+      id: Number
+    },
+    countries: [String],
+    type: String,
+    tomatoes: {
+      viewer: {
+        rating: Number,
+        numReviews: Number,
+        meter: Number
+      },
+      lastUpdated: Date
+    }
+  });
 
-const Movie = mongoose.model('Movie', movieSchema);
+  const Movie = mongoose.model('Movie', movieSchema);
 
-module.exports = Movie;
+  module.exports = Movie;
